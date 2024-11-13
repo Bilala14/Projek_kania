b4ff6066d9f567bdc69e8de476a2bde0d614f34e
Bilala14 <kanianabilamuntaz14@gmail.com>
2024-11-13 16:50:58 +0700

    penyesuaian tampilan index

--- modul_jurusan/index.php
+++ modul_jurusan/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Jurusan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
 </head>
 <body>
 <?php include_once('../navbar.php');
@@ -13,19 +14,40 @@
     <div class="row mt-5">
         <div class="col-6 m-auto"></div>
             <div class="card">
-    <div class="card-header" >
+    <div class="card-header text-center" >
        <h3>Data Jurusan</h3>
+       <span class="float-end"><a class="btn btn-primary" href=""><i class="fa-solid fa-square-plus"></i> Tambah Data</a></span>
     </div>
     <div class="card-body">
-      <h4>Selamat Datang,Administrator</h4>
-      <p>Tetap Semangat</p>
+    <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Kode</th>
+      <th scope="col">Jurusn</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>
+        <a class="btn btn-info btn-sm" href=""><i class="fa fa-pen-to-square"></i></a>
+        <a class="btn btn-danger btn-sm" href=""><i class="fa fa-delete-left"></i></a>
+      </td>
+    </tr>
+  </tbody>
+</table>
     </div>
     </div>
     </div>
     </div>
     </div>
 
-    <script>src="../js/bootstrap.js"</script> 
-    <script>src="../js/bootstrap.bundle.js"</script> 
+    <script src="../js/bootstrap.js"></script> 
+    <script src="../js/bootstrap.bundle.js"></script> 
+    <script src="../js/all.js"></script> 
 </body>
 </html>
