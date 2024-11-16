3cbe3d69adafcf5830472b00ee590f152deae0fb
Bilala14 <kanianabilamuntaz14@gmail.com>
2024-11-16 10:36:48 +0700

    proses tambah dan tampil data jurusan

--- modul_jurusan/index.php
+++ modul_jurusan/index.php
@@ -16,7 +16,7 @@
             <div class="card">
     <div class="card-header text-center" >
        <h3>Data Jurusan</h3>
-       <span class="float-end"><a class="btn btn-primary" href=""><i class="fa-solid fa-square-plus"></i> Tambah Data</a></span>
+       <span class="float-end"><a class="btn btn-primary" href="form.php"><i class="fa-solid fa-square-plus"></i> Tambah Data</a></span>
     </div>
     <div class="card-body">
     <table class="table table-striped">
@@ -24,20 +24,37 @@
     <tr>
       <th scope="col">No</th>
       <th scope="col">Kode</th>
-      <th scope="col">Jurusn</th>
+      <th scope="col">Jurusan</th>
       <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
+    <?php
+    #1 Membuat Koneksi  database dengan php
+    include("../koneksi.php");
+
+    #2 Menuliskan query simpan
+    $tampil = "SELECT * FROM jurusans";
+
+    #3 Menjalankan query
+    $proses = mysqli_query($koneksi,$tampil);
+
+    #4 Looping data dari database
+    $nomor =1;
+    foreach($proses as $data){
+    ?>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
+      <th scope="row"><?=$nomor++?></th>
+      <td><?=$data['kode']?></td>
+      <td><?=$data['jurusan']?></td>
       <td>
         <a class="btn btn-info btn-sm" href=""><i class="fa fa-pen-to-square"></i></a>
         <a class="btn btn-danger btn-sm" href=""><i class="fa fa-delete-left"></i></a>
       </td>
     </tr>
+    <?php
+    }
+    ?>
   </tbody>
 </table>
     </div>
